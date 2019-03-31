f68f89e8441b9f2c023c6d077e5be2df2bef422e
bkRahul <rahulsatya1068@gmail.com>
2019-03-31 23:34:57 +0530

    added bulma css framework

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield("title", "Laracasts")</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
     <script src="main.js"></script>
 </head>
