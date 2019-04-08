81f06da057875205dde7490d2420e2e875249b0d
bkRahul <rahulsatya1068@gmail.com>
2019-04-08 22:45:51 +0530

    added checkbox to check finished tasks

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,24 +1,36 @@
 @extends('projects.projectlayout')
 
 @section('content')
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="m-b-md">
-                    <h3>{{ $project->title }}</h3>
-                </div>
-                <div class="sub-title m-b-md">
-                    <h5>{{ $project->description }}</h5>
-                </div>
 
-                @if($project->tasks->count())
-                <div>
-                    @foreach($project->tasks as $task)
-                        <p>{{$task->description}}</p>
-                    @endforeach
-                </div>
-                @endif
-
-                 <div><a href="{{ $project->id }}/edit">Edit</a></div>
-            </div>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="m-b-md">
+            <h3>{{ $project->title }}</h3>
         </div>
+        <div class="sub-title m-b-md">
+            <h5>{{ $project->description }}</h5>
+        </div>
+
+        @if($project->tasks->count())
+        <div>
+        @foreach($project->tasks as $task)
+
+        <form method="POST" action="/tasks/{{ $task->id }}">
+            {{ method_field('PATCH') }}
+            {{ csrf_field() }}
+            <label class="checkbox {{( $task->completed ? 'iscomplete' : '' )}}" for="completed">
+                <input type="checkbox" name="completed" onchange="this.form.submit()" {{( $task->completed ? 'checked' : '' )}}>            
+                    {{$task->description}}
+            </label>
+        </form>
+
+        @endforeach
+
+        </div>
+        @endif
+
+         <div><a href="{{ $project->id }}/edit">Edit</a></div>
+    </div>
+</div>
+
 @endsection('content')
