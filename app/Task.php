91e756dacc064e66001e93aae9be92d8dfb0d2cf
bkRahul <rahulsatya1068@gmail.com>
2019-04-29 23:45:16 +0530

    added complete and incomplete functions

--- app/Task.php
+++ app/Task.php
@@ -6,10 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['completed', 'description'];
-    
+   protected $fillable = ['completed', 'description'];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
     }
+    
+	public function complete( $completed =true ) {
+	$this->update([
+            'completed' => $completed
+        ]);
+	}       
+
+	public function incomplete() {
+	$this->complete(false);
+	}  
+
 }
